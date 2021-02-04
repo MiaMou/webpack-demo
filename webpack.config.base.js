04759afc68da217b40d7ac22da6bc951e730ac01
MiaMou <19845509@qq.com>
2021-02-04 13:08:34 +0800

    upload

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,5 +11,57 @@ module.exports = {
       title: 'XDML - 写代码啦',
       template: 'src/assets/index.html'
     }),
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'   // use: ["file-loader"]
+      }, 
+      {
+        test: /\.styl$/,
+        use: [
+          {
+            loader: "style-loader", // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+          {
+            loader: "stylus-loader", // compiles Stylus to CSS
+          },
+        ],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          {
+            loader: "style-loader", // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+          {
+            loader: "less-loader", // compiles Less to CSS
+          },
+        ],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require('dart-sass')
+            }
+          }
+        ]
+      }
+    ],
+  },
 };
